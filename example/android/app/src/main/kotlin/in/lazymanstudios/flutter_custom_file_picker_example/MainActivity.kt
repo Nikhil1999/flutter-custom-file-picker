4a1743b212d15f9f1278ecd3b8b1ae465d6b7141
Nikhil1999 <nikhilkoshty@gmail.com>
2021-06-07 16:48:05 +0530

    Added Pick File

---
+++ example/android/app/src/main/kotlin/in/lazymanstudios/flutter_custom_file_picker_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package in.lazymanstudios.flutter_custom_file_picker_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/flutter_custom_file_picker.dart
+++ lib/flutter_custom_file_picker.dart
@@ -1,14 +1,17 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class FlutterCustomFilePicker {
-  static const MethodChannel _methodChannel =
-      const MethodChannel('in.lazymanstudios.customfilepickerplugin/customfilepicker');
+  static const MethodChannel _channel = const MethodChannel(
+      'in.lazymanstudios.customfilepickerplugin/customfilepicker');
 
-  static Future<bool> test() async {
-    bool result = await _methodChannel.invokeMethod('test');
-    return result;
+  static Future<String> test() async {
+    return await _channel.invokeMethod('test');
+  }
+
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
   }
 }
